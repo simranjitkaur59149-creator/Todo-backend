cd952fa9b894f2ced25b599ade2c747b8590f747
simran <simranjitkaur59149@gamil.com>
2026-04-07 01:09:38 +0530

    url updated

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
-import mongoDb from "./config/database.js"
-import routes from "./routes/todoRoutes.js"
-const PORT=process.env.PORT || 8000
-dotenv.config()
-const app=express()
-mongoDb()
-app.use(cors({
-    origin:"http://todos-fronted.vercel.app/"
-}))
-app.use(express.json())
-app.use("/app",routes)
-app.listen((PORT),()=>console.log("Server is running"))
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoDb from "./config/database.js";
+import routes from "./routes/todoRoutes.js";
+const PORT = process.env.PORT || 8000;
+dotenv.config();
+const app = express();
+mongoDb();
+app.use(
+  cors({
+    origin: "http://todos-fronted.vercel.app/",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+app.use(express.json());
+app.use("/app", routes);
+app.listen(PORT, () => console.log("Server is running"));
